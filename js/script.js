6e193011f52962c8af9178af50776d9b6b2e01e4
Trinity John <trinityrtime@gmail.com>
2019-10-28 17:27:13 +0300

    correct ids assigned ;and correct assigning in script.js

--- js/script.js
+++ js/script.js
@@ -43,26 +43,35 @@ Day of the week (d) = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD 
  DD - is the Day of the month 
 
  mod - is the modulus function ( % )*/
+
  function Akancalculator(){
   var date1 = document.getElementById("db").value;
   // alert(date1);
-  var ged1 = document.getElementById("ged").value;
+  var ged1 = document.getElementById("ged");
+  var ged2 = document.getElementById("ged2");
+  //  for (var n = 0, ln = ged1.length; n < ln; n++){
+  //     if(ged1[n].checked){
+  //       var genderfinal = ged1[n].value;
+  //       alert(genderfinal);
+  //     }
+      // else{
+      //   genderfinal = ged1[n].value;
+      // }
+   
   
-  var date2 = parseInt(date1.toString()); 
+  var date2 = date1.toString(); 
   var cc = parseInt(date2.slice(0,2));
   var yy = parseInt(date2.slice(2,4));
   var mm = parseInt(date2.slice(5,7));
   var dd = parseInt(date2.slice(8,10));
   
   var dow = Math.trunc (( ( (cc/4) -2*cc-1) + ((5*yy/4) ) + ((26*(mm+1)/10)) + dd ) % 7);
-  alert(dow);
-
-  var gendfemale = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
-  var gendmale = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
-  if (ged1 === "Male"){
+  var gendfemale = ["Ama","Akosua","Adwoa","Abenaa","Akua","Yaa","Afua"];
+  var gendmale = ["Kwame","Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi"];
+  if (ged1.checked==true){
     alert(gendmale[dow])
   }
-  else if(ged1 === "Female"){
+  else if(ged2.checked==true){
   alert(gendfemale[dow])
   }
   else{
