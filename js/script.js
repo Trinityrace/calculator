033666b0ab01a444fbaab1b36a837f8d077ed237
Trinity John <trinityrtime@gmail.com>
2019-10-28 15:39:14 +0300

    day of week(dow) formula insert

--- js/script.js
+++ js/script.js
@@ -53,5 +53,7 @@ Day of the week (d) = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD 
   var yy =date2.slice(2,4);
   var mm =date2.slice(5,7);
   var dd =date2.slice(8,10);
+  
+  var dow = ( ( (cc/4) -2*cc-1) + ((5*yy/4) ) + ((26*(mm+1)/10)) + dd ) % 7
   alert(date2);
  }
